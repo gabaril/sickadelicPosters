f317705ed362a1e9326f7bc8812b16dcc8cca877
Gabriel Baril <Gabriel Baril>
2017-04-17 19:53:36 -0400

    ajout image et debut peuple bd

--- src/php/ApiFetchSpecific.php
+++ src/php/ApiFetchSpecific.php
@@ -5,7 +5,8 @@
  * Date: 17-02-22
  * Time: 13:49
  */
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: http://localhost:3000 ");
+//header("Access-Control-Allow-Origin: http://decorator-arches-17418.netlify.com");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization, If-Modified-Since, Cache-Control, Pragma");
 
 require_once ('config.php');
